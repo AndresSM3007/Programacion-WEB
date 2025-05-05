575c3015b859d40632f5306437c8ab33e70c6627
AndresSM3007 <l21230625@minatitlan.tecnm.mx>
2025-05-04 21:55:49 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'mysql.webcindario.com';  // Servidor de Webcindario
 $dbname = 'apoya_tec';           // Nombre de tu base de datos
-$username = 'tu_usuario';        // Usuario de la base de datos (proporcionado por Webcindario)
-$password = 'tu_contraseña';     // Contraseña de la base de datos
+$username = 'apoya_tec';        // Usuario de la base de datos (proporcionado por Webcindario)
+$password = 'AndresSM30$$$';     // Contraseña de la base de datos
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
